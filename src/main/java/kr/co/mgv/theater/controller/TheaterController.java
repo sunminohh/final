3e1c0364a97427c2f7d7e7ecfadb1f334dc8add4
hyungook <hgwls88@gmail.com>
2023-07-30 02:17:59 +0900

    Style: 극장 레이아웃 추가

--- src/main/java/kr/co/mgv/theater/controller/TheaterController.java
+++ src/main/java/kr/co/mgv/theater/controller/TheaterController.java
@@ -1,15 +1,22 @@
 package kr.co.mgv.theater.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/theater")
 public class TheaterController {
 
-    @RequestMapping({"/", ""})
+    @GetMapping({"/", ""})
     public String home() {
         return "/view/theater/home";
+    }
+
+    @GetMapping("/detail")
+    public String detail(@RequestParam(defaultValue = "1") String brchNo) {
+        return "/view/theater/detail";
     }
 
 }
