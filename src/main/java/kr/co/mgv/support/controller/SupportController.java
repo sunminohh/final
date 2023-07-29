31d4aa7505c9e83876d8565e05963fa4d0c0bfaa
오민선 <minsunooh@gmail.com>
2023-07-29 18:40:11 +0900

    Style: 고객센터 레이아웃 추가

--- src/main/java/kr/co/mgv/support/controller/SupportController.java
+++ src/main/java/kr/co/mgv/support/controller/SupportController.java
@@ -1,7 +1,9 @@
 package kr.co.mgv.support.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/support")
@@ -12,4 +14,33 @@ public class SupportController {
         return "/view/support/home";
     }
 
+    @GetMapping("/faq")
+    public String feq() {
+        return "/view/support/faq/list";
+    }
+
+    @GetMapping("/notice")
+    public String notice() {
+        return "/view/support/notice/list";
+    }
+
+    @GetMapping("/notice/detail")
+    public String noticeDetail(@RequestParam(defaultValue = "1") int noticeNo) {
+        return "/view/support/notice/detail";
+    }
+
+    @GetMapping("/oneonone")
+    public String oneonone() {
+        return "/view/support/oneonone/list";
+    }
+
+    @GetMapping("/terms")
+    public String terms() {
+        return "/view/support/terms/list";
+    }
+
+    @GetMapping("/privacy")
+    public String privacy() {
+        return "/view/support/privacy-policy/list";
+    }
 }
