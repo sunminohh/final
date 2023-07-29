f99672b9ee063e274a69e5531bd9e9af2542e5ea
Joonho Seo <joonhoseo1991@gmail.com>
2023-07-30 01:47:52 +0900

    Style: 영화, 예매 레이아웃 추가

--- src/main/java/kr/co/mgv/movie/controller/MovieController.java
+++ src/main/java/kr/co/mgv/movie/controller/MovieController.java
@@ -3,6 +3,7 @@ package kr.co.mgv.movie.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/movie")
@@ -11,6 +12,11 @@ public class MovieController {
     @RequestMapping({"/", ""})
     public String home() {
         return "/view/movie/home";
+    }
+
+    @GetMapping("/detail")
+    public String detail(@RequestParam String movieNo) {
+        return "/view/movie/detail";
     }
 
     @GetMapping("/post")
