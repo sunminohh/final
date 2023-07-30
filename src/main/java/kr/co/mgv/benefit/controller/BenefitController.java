360ab40224036fc028e4665f5f8aea3b2d5279c1
Shinhawo <hownu080@naver.com>
2023-07-30 20:45:45 +0900

    Style: 혜택 레이아웃 구성

---
+++ src/main/java/kr/co/mgv/benefit/controller/BenefitController.java
@@ -0,0 +1,23 @@
+package kr.co.mgv.benefit.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/benefit")
+public class BenefitController {
+
+    @GetMapping({"", "/"})
+    public String membershipHome() {
+        return "/view/benefit/membership";
+    }
+
+    @GetMapping("/discount")
+    public String discountHome() {
+        return "/view/benefit/discount";
+    }
+
+}
