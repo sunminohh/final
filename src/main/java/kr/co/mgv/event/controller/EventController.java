e0c33dd6865e1f58ab80038f6c41d3f783dc935a
오민선 <minsunooh@gmail.com>
2023-07-30 20:46:23 +0900

    Style: 이벤트, 고객센터 레이아웃

--- src/main/java/kr/co/mgv/event/controller/EventController.java
+++ src/main/java/kr/co/mgv/event/controller/EventController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -16,10 +17,25 @@ public class EventController {
         return "/view/event/home";
     }
 
-    @GetMapping("/list/{cateNo}")
-    public String list(@PathVariable String cateNo) {
+    @GetMapping("/list")
+    public String list(@RequestParam(defaultValue = "1") String cateNo) {
         log.info("[Event] - list: {}", cateNo);
         return "/view/event/list";
+    }
+
+    @GetMapping("/end")
+    public String endList() {
+        return "/view/event/end-list";
+    }
+
+    @GetMapping("/winner/list")
+    public String winnerList() {
+        return "/view/event/winner-list";
+    }
+
+    @GetMapping("/winner/detail")
+    public String winnerList(@RequestParam String eventNo) {
+        return "/view/event/winner-detail";
     }
 
     @GetMapping("/detail/{eventNo}")
--- src/main/java/kr/co/mgv/store/controller/StoreController.java
+++ src/main/java/kr/co/mgv/store/controller/StoreController.java
@@ -1,15 +1,22 @@
 package kr.co.mgv.store.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/store")
 public class StoreController {
 
-    @RequestMapping({"/", ""})
+    @GetMapping({"/", ""})
     public String home() {
         return "/view/store/home";
+    }
+
+    @GetMapping("/detail")
+    public String detail(@RequestParam(defaultValue = "1") String storeNo) {
+        return "/view/store/detail";
     }
 
 }
