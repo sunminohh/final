2dbcacae0d545aab245372e48851f8687a02ad16
신하원 <hownu080@naver.com>
2023-07-31 17:44:59 +0900

    Feat 극장게시판 상세페이지 리플 html

--- src/main/resources/static/js/board/detail.js
+++ src/main/resources/static/js/board/detail.js
@@ -1,55 +1,64 @@
 /**
  * 좋아요 버튼 관련 코드
  */
-$(function() {
-	let likeButtonClicked = false;
-	$("#like-button").click(function(event) {
-		event.preventDefault();
-		if (!likeButtonClicked) {
-			$(this).removeClass("btn-outline-secondary").addClass("btn-secondary");
-			likeButtonClicked = true;
-		} else {
-			$(this).removeClass("btn-secondary").addClass("btn-outline-secondary");
-			likeButtonClicked = false;
-		}
+	$(function() {
+		let likeButtonClicked = false;
+		$("#like-button").click(function(event) {
+			event.preventDefault();
+			if (!likeButtonClicked) {
+				$(this).removeClass("btn-outline-secondary").addClass("btn-secondary");
+				likeButtonClicked = true;
+			} else {
+				$(this).removeClass("btn-secondary").addClass("btn-outline-secondary");
+				likeButtonClicked = false;
+			}
 	})
 
-$(function() {
-	
-	$("#comment-box").click(function(event) {
-		event.preventDefault();		
+	$(function() {
 		
-
-		let content = `
-	   				<div id="profile" >
-						<div id="comment-imgbox" >
-							<a href="사용자상세정보">
-								<img id="comment-profileimg" th:src="@{/images/board/sample.png}" alt="프로필사진">
-							</a>
+		$("#a-reply").click(function(event) {
+			event.preventDefault();		
+	
+			let content = `
+		   	<div class="col-11 ms-5 CommentWriter">
+				<div class="comment-inbox border p-2 rounded"> 
+	
+					<em class="comment_inbox_name">로그인된 아이디</em>
+					
+					<form class="" method="post" action="insertComment.jsp">
+						<input type="hidden" name="no" value= />
+		 				<div class="row">
+							<div >
+								<textarea rows="2" class="comment_inbox_text" name="content" style="border: none; overflow: hidden; overflow-wrap: break-word; " 
+								placeholder="댓글을 남겨보세요"></textarea>
+							</div>
+							<div class="register_box">
+								<button class="btn btn-outline-white btn-sm float-end" style="border: none">등록</button>
+							</div>
 						</div>
-	
-					</div>
-					<div class="commentUserInfo ps-5" >
-							<p>댓글쓴놈 아이디</p> 
-		   					<p>내용이요</p>
-		   					<div class="d-flex justify-content-start">
-		   						<p class="float-start me-2" style="font-size: 12px; color: gray">1998/06/24 07:00:00</p>
-		   						<a href="" class="float-satrt" style="text-decoration:none; font-size: 12px; color: gray">답글쓰기</a>
-		   					</div>
-						
-					</div>
-		   			<div >
-		   				
-		   				<a  th:href="@{/deleteComment(no=1, cno=1)}" 
-		   					class="btn btn-link text-danger text-decoration-none float-end"><i class="bi bi-trash"></i></a>
-	
-	   				</div>   			
-		`
-	
+					</form>   	
+				</div>
+	   		</div>  			
+			`;
+
 			$("#reply-comment-box").append(content)
+			$("#a-reply").attr('id', 'reply').text('취소')
+			
+		})
 	})
-})
+	
+	
 
-
+	$(function() {
+		
+		$("#reply").click(function(event) {
+			event.preventDefault();	
+			
+			$("#reply-comment-box").html(" ")
+			$("#reply").attr('id', 'a-reply').text('답글쓰기')
+		
+		})
+		
+	})
 	
 })
