ad7aee597d273d7ca290dfed2084526d13f5011b
신하원 <hownu080@naver.com>
2023-08-01 15:10:42 +0900

    Feat form INSERT를 위한 summerNote js 수정

--- src/main/resources/static/js/summernote/sn.js
+++ src/main/resources/static/js/summernote/sn.js
@@ -9,7 +9,35 @@ $(document).ready(function() {
 		  maxHeight: null,             // 최대 높이
 		  focus: true,                  // 에디터 로딩후 포커스를 맞출지 여부
 		  lang: "ko-KR",					// 한글 설정
-		  placeholder: '내용을 입력하세요 오민선 바보'	//placeholder 설정
+		  placeholder: '내용을 입력하세요.',	//placeholder 설정
+		  toolbar: [
+	       // 글꼴 설정
+	       ['fontname', ['fontname']],
+	       // 글자 크기 설정
+	       ['fontsize', ['fontsize']],
+	       // 굵기, 기울임꼴, 밑줄,취소 선, 서식지우기
+	       ['style', ['bold', 'italic', 'underline','strikethrough', 'clear']],
+	        // 글자색
+	       ['color', ['forecolor','color']],
+	       // 표만들기
+	       ['table', ['table']],
+	       // 글머리 기호, 번호매기기, 문단정렬
+	       ['para', ['ul', 'ol', 'paragraph']],
+	       // 줄간격
+	       ['height', ['height']],
+	       // 코드보기, 확대해서보기, 도움말
+	       ['view', ['codeview','fullscreen', 'help']]
+        ],
+        // 추가한 글꼴
+        fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New','맑은 고딕','궁서','굴림체','굴림','돋음체','바탕체'],
+        // 추가한 폰트사이즈
+        fontSizes: ['8','9','10','11','12','14','16','18','20','22','24','28','30','36','50','72']
           
 	});
+	
+	
+	 
+       
+
+   
 });
