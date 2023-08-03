2dac76d8e046db3182c4120bab127d69923abf45
hyungook <hgwls88@gmail.com>
2023-08-03 14:08:20 +0900

    Feat 상영시간표 날짜버튼 생성 기능

--- src/main/resources/static/js/theater/ui.js
+++ src/main/resources/static/js/theater/ui.js
@@ -1,4 +1,39 @@
 $(() => {
+	function refreshDate(){
+		dayjs.locale('ko-kr')
+		let today = dayjs();
+		let targetDay = today.add(13, 'day');
+		let htmlContents = `<div class="year" style="left: 30px; z-index: 1; opacity: 1;">${today.get("y")}.${today.get("M")+1}</div>`;
+		dayjs.extend(dayjs_plugin_isSameOrAfter)
+		if(!dayjs(today).isSameOrAfter(targetDay, 'month')){
+			htmlContents += `<div class="year" style="left: 30px; z-index: 1; opacity: 0;">${targetDay.get("y")}.${targetDay.get("M")+1}</div>`;
+		}
+		$(".year-area").html(htmlContents);
+		htmlContents=``;
+		let currentDay = today.clone();
+		let index = parseInt(today.get('d'));
+		let target = parseInt(targetDay.get('d'));
+		for(i =1; i < 13 ;i++){
+			let weekDay = currentDay.format('dd');
+			if(currentDay.diff(toady, 'day')==0){
+				weekDay = '오늘';
+			}
+			if(currentDay.diff(toady, 'day')==1){
+				weekDay = '내일';
+			}
+			htmlContents += `<button class="on" type="button" date-data="${currentDay.format('YYYY.MM.DD')}"
+											month="7">
+											<span class="ir">${currentDay.format('YYYY년MM월')}</span><em
+												style="pointer-events: none;">${currentDay.get('day')}<span
+												style="pointer-events: none;" class="ir">일</span></em><span
+												class="day-kr"
+												style="pointer-events: none; display: inline-block">${weekDay}</span><span
+												class="day-en" style="pointer-events: none; display: none">Tue</span>
+										</button>`;
+			currentDay.add(1,'day');
+		}
+		$(".date-area .wrap").html(htmlContents);
+	}
 	// 극장 상세 탭 버튼 클릭시 
     $(".tab-list a").on("click", function(){
 		// 다른 버튼은 비활성화
@@ -11,6 +46,7 @@ $(() => {
 		$(tabId).siblings().removeClass("on");
 		// 탭 아이디로 탭을 찾아서 활성화
 		$(tabId).addClass("on");
+		refreshDate();
 	})
 	
 	$(".theater-place button").on("click", function(){
