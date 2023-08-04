231e115bd6c687dc5357e4184e1cac376ddcf9d7
MooingcowJoon <joonhoseo1991@gmail.com>
2023-08-04 09:54:05 +0900

    Feat MovieLikesMapper 생

--- src/main/java/kr/co/mgv/movie/mapper/MovieLikesMapper.java
+++ src/main/java/kr/co/mgv/movie/mapper/MovieLikesMapper.java
@@ -1,2 +1,13 @@
-package kr.co.mgv.movie.mapper;public interface MovieLikesMapper {
+package kr.co.mgv.movie.mapper;
+
+import kr.co.mgv.movie.vo.MovieLikes;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Set;
+
+@Mapper
+public interface MovieLikesMapper {
+    public void insertMovieLikes(MovieLikes movieLikes);
+    public Set<Integer> getLikedMovieNosByUserId(String userId);
+    public void deleteMovieLikes(MovieLikes movieLikes);
 }
