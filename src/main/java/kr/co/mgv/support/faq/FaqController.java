dd2a32e8efeb1739ffcf3fe0f5288af56e6e98e0
오민선 <minsunooh@gmail.com>
2023-08-07 16:06:03 +0900

    Feat 고객센터 faq EmptyList 수정

--- src/main/java/kr/co/mgv/support/faq/FaqController.java
+++ src/main/java/kr/co/mgv/support/faq/FaqController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 
 @Controller
