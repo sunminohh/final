489a17b557e3aed1840608bc7c70956736a7f26d
오민선 <minsunooh@gmail.com>
2023-08-07 15:41:32 +0900

    Feat 고객센터 faq 리스트 수정

--- src/main/java/kr/co/mgv/support/faq/FaqController.java
+++ src/main/java/kr/co/mgv/support/faq/FaqController.java
@@ -4,6 +4,7 @@ package kr.co.mgv.support.faq;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,18 +16,24 @@ import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/support/faq")
-@Slf4j
+@RequiredArgsConstructor
 public class FaqController {
 
+	private final FaqService faqService;
+	
 	@GetMapping
-	public String faq() {
+	public String faq(Model model) {
+		List<Faq> faqList = faqService.getFaqList(1);
+		model.addAttribute("faqList", faqList);
+		
 		return "/view/support/faq/list";
 	}
 	
 	@GetMapping("/list")
 	@ResponseBody
 	public List<Faq> getFaq(@RequestParam("catNo") int catNo) {
+		List<Faq> faqList = faqService.getFaqList(catNo);
 		
-		return null;
+		return faqList;
 	}
 }
