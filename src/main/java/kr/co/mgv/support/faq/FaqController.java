a438e29b4825ca33bf348d819565f85912bab394
오민선 <minsunooh@gmail.com>
2023-08-08 17:38:23 +0900

    Feat 고객센터 Faq 페이지네이션, 검색기능 추가

--- src/main/java/kr/co/mgv/support/faq/FaqController.java
+++ src/main/java/kr/co/mgv/support/faq/FaqController.java
@@ -1,10 +1,13 @@
 package kr.co.mgv.support.faq;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,16 +25,30 @@ public class FaqController {
 	
 	@GetMapping
 	public String faq(Model model) {
-		List<Faq> faqList = faqService.getFaqList(1);
-		model.addAttribute("faqList", faqList);
+		Map<String, Object> param = new HashMap<>();
+		param.put("catNo", 1);
+		param.put("page", 1);
+		FaqList faqList = faqService.search(param);
+		
+		model.addAttribute("result", faqList);
 		
 		return "/view/support/faq/list";
 	}
 	
 	@GetMapping("/list")
 	@ResponseBody
-	public List<Faq> getFaq(@RequestParam("catNo") int catNo) {
-		List<Faq> faqList = faqService.getFaqList(catNo);
+	public FaqList getFaq(@RequestParam(name = "catNo") int catNo,
+			@RequestParam(name = "page", required = false, defaultValue = "1") int page,
+			@RequestParam(name = "keyword", required = false) String keyword) {
+		
+		Map<String, Object> param = new HashMap<>();
+		param.put("catNo", catNo);
+		param.put("page", page);
+		if (StringUtils.hasText(keyword)) {
+			param.put("keyword", keyword);
+		}
+		
+		FaqList faqList = faqService.search(param);
 		
 		return faqList;
 	}
