af19138a04ee7044ce054832ac35de83970eeae6
신하원 <hownu080@naver.com>
2023-08-10 16:27:47 +0900

    Fix 충돌해결

--- src/main/java/kr/co/mgv/theater/Theater.java
+++ src/main/java/kr/co/mgv/theater/Theater.java
@@ -3,12 +3,9 @@ package kr.co.mgv.theater;
 import java.util.List;
 
 import org.apache.ibatis.type.Alias;
-<<<<<<< HEAD
-=======
 
 import kr.co.mgv.theater.facility.TheaterFacility;
 import kr.co.mgv.theater.floors.FloorInfo;
->>>>>>> develop
 import kr.co.mgv.theater.location.Location;
 import kr.co.mgv.theater.parking.ParkingInfo;
 import lombok.Getter;
@@ -27,18 +24,13 @@ public class Theater {
 	private String address;
 	private String tel;
 	private String disabled;
-<<<<<<< HEAD
-	private String parkingInfo;
-	private String facilityInfo;
-	private String description;
-	
-	public Theater(int no) {
-		this.no = no;
-	}
-=======
 	private String info;
 	private List<TheaterFacility> facilities;
 	private List<FloorInfo> floorInfos;
 	private ParkingInfo parkingInfo;
->>>>>>> develop
+	
+	public Theater(int no) {
+		this.no = no;
+	}
+
 }
