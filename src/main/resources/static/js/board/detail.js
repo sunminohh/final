455bdb2dbdcc87cf086939e1630de2a7cafd10b2
신하원 <hownu080@naver.com>
2023-08-14 12:37:19 +0900

    Feat 영화 게시판 공유 버튼

--- src/main/resources/static/js/board/detail.js
+++ src/main/resources/static/js/board/detail.js
@@ -363,6 +363,29 @@ $(function() {
 	    });
 	});
 
+	$("#link-btn").on("click", function() {
+	    // 현재 페이지의 URL을 클립보드에 복사
+	    navigator.clipboard.writeText(window.location.href)
+	        .then(() => {
+	            // 복사 성공 시 팝오버를 표시합니다.
+	            $("#link-btn").popover('show');
+	            
+	            // 2초 후에 팝오버를 숨깁니다.
+	            setTimeout(function() {
+	                $("#link-btn").popover('hide');
+	            }, 2000);
+	        })
+	        .catch((error) => {
+	            console.error("URL 복사 중 오류가 발생했습니다.", error);
+	            // 복사 실패 시에 수행할 작업을 여기에 추가할 수 있습니다.
+	        });
+	});
+	
+	// 페이지 로딩 시에 팝오버 초기화
+	$(function () {
+	    $('[data-bs-toggle="popover"]').popover();
+	});
+
 
 })
 
