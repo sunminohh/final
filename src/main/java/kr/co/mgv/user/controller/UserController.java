037fa0382ca2a920e64bdc643fb0c5e4a5d3272d
hia0706 <ljjs1215@gmail.com>
2023-08-17 23:00:04 +0900

    Feat 내정보 수정

--- src/main/java/kr/co/mgv/user/controller/UserController.java
+++ src/main/java/kr/co/mgv/user/controller/UserController.java
@@ -1,16 +1,37 @@
 package kr.co.mgv.user.controller;
 
+import kr.co.mgv.user.vo.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/user/info")
+@Slf4j
 public class UserController {
-    @RequestMapping({"/", ""})
+
+    @GetMapping("/home")
+    @ResponseBody
+    public ResponseEntity<String> home(@AuthenticationPrincipal User user) {
+        if (user != null) {
+            log.info("사용자 -> {}", user);
+            return ResponseEntity.ok("authenticated");
+        } else {
+            log.info("사용자 -> {}", (Object) null);
+            return ResponseEntity.ok("anonymous");
+        }
+    }
+    // todo 기존 코드
+    /*
+    @RequestMapping({"/",""})
     public String home() {
         return "view/user/home";
     }
+    */
 
     @GetMapping("/booking")
     public String booking() {
