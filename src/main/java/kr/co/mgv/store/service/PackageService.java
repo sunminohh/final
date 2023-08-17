df1130ffc893cc4fc41f234235e4b26f0c329b45
jaekwon Lee <ljg10sky@naver.com>
2023-08-17 16:33:51 +0900

    Feat 상품 및 패키지 Service에 전체조회 및 번호로 찾는 기능 추가

--- src/main/java/kr/co/mgv/store/service/PackageService.java
+++ src/main/java/kr/co/mgv/store/service/PackageService.java
@@ -5,6 +5,8 @@ import kr.co.mgv.store.vo.Package;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PackageService {
@@ -14,5 +16,13 @@ public class PackageService {
     public void insertPackage(Package pkg) {
 
         packageMapper.insertPackage(pkg);
+    }
+
+    public Package getPackageByNo(int no) {
+        return packageMapper.getPackageByNo(no);
+    }
+
+    public List<Package> getAllPackages() {
+        return packageMapper.getPackages();
     }
 }
