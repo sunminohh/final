aad459548ab54184bd8d960642585270c8b21460
hia0706 <ljjs1215@gmail.com>
2023-08-18 02:45:02 +0900

    Style 주석 제거

--- src/main/java/kr/co/mgv/user/controller/UserController.java
+++ src/main/java/kr/co/mgv/user/controller/UserController.java
@@ -1,13 +1,9 @@
 package kr.co.mgv.user.controller;
 
-import kr.co.mgv.user.vo.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user/info")
