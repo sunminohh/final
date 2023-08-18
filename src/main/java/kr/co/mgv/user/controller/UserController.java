db036a7af2a32e37442e7caa6054e58997d64e23
hia0706 <ljjs1215@gmail.com>
2023-08-18 11:44:56 +0900

    Feat 회원 정보 수정

--- src/main/java/kr/co/mgv/user/controller/UserController.java
+++ src/main/java/kr/co/mgv/user/controller/UserController.java
@@ -1,18 +1,40 @@
 package kr.co.mgv.user.controller;
 
+import kr.co.mgv.user.service.AuthenticationService;
+import kr.co.mgv.user.service.EmailServiceImpl;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Secured("ROLE_USER")
 @RequestMapping("/user/info")
+@RequiredArgsConstructor
 @Slf4j
 public class UserController {
+
+    private final AuthenticationService authenticationService;
+    private final EmailServiceImpl emailService;
 
     @RequestMapping({"/",""})
     public String home() {
         return "view/user/home";
+    }
+
+    // 이메일 인증
+    @GetMapping("/auth")
+    public String pwdCheckForm() {
+        return "view/user/info/auth";
+    }
+
+    // 이메일 인증 후 회원정보 폼
+    @GetMapping("/form")
+    public String info() {
+
+        return "view/user/info/form";
     }
 
     @GetMapping("/booking")
@@ -36,14 +58,5 @@ public class UserController {
         return "view/user/inquiry/list";
     }
 
-    @GetMapping("/myinfo")
-    public String myinfo() {
-        return "view/user/myinfo/form";
-    }
 
-    // 나의 정보
-    @GetMapping("/myinfo/pwdform")
-    public String pwdform() {
-        return "view/user/myinfo/pwdform";
-    }
 }
