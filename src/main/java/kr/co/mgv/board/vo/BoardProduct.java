87a7e66d5d98a1c4150c6710b32f645a2ca3fc90
신하원 <hownu080@naver.com>
2023-08-18 16:55:00 +0900

    Feat 스토어게시판 게시물 등록 기능 구현

--- src/main/java/kr/co/mgv/board/vo/BoardProduct.java
+++ src/main/java/kr/co/mgv/board/vo/BoardProduct.java
@@ -1,5 +1,6 @@
 package kr.co.mgv.board.vo;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,4 +13,12 @@ public class BoardProduct {
 	private int no;
 	private String name;
 	private BoardCategory category;
+
+	@Builder
+	public BoardProduct(int no, String name, BoardCategory category) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.category = category;
+	}
 }
