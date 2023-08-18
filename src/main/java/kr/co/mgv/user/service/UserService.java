d3eeebe8af31e8598f95b9249da71b89667567b1
hia0706 <ljjs1215@gmail.com>
2023-08-18 11:44:56 +0900

    Feat 회원 정보 수정

---
+++ src/main/java/kr/co/mgv/user/service/UserService.java
@@ -0,0 +1,10 @@
+package kr.co.mgv.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+}
