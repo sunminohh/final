7426dc753197ff71d2a7bb7fb7ca39c3208910a0
신하원 <hownu080@naver.com>
2023-08-21 15:13:51 +0900

    Feat 파티게시판 등록 폼 모달 적용

--- src/main/java/kr/co/mgv/board/controller/PartyController.java
+++ src/main/java/kr/co/mgv/board/controller/PartyController.java
@@ -1,6 +1,7 @@
 package kr.co.mgv.board.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
@@ -9,11 +10,20 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.co.mgv.board.service.TheaterBoardService;
+import kr.co.mgv.board.vo.BoardLocation;
+import kr.co.mgv.board.vo.BoardTheater;
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/board/party")
+@RequiredArgsConstructor
 public class PartyController {
 
+		private final TheaterBoardService theaterBoardService;
+	
 	   @GetMapping("/list")
 	    public String theaterList(@RequestParam(name = "sort", required = false, defaultValue = "id") String sort,
 				@RequestParam(name = "rows", required = false, defaultValue = "10") int rows,
@@ -51,8 +61,19 @@ public class PartyController {
 	        return "/view/board/party/detail";
 	    }
 
+	    
+	    // 게시물 등록 관련
+		@GetMapping("/theaterByLocationNo")
+		@ResponseBody
+		public List<BoardTheater> getTheaterbyLocationNo(@RequestParam("locationNo") int locationNo) {
+			List<BoardTheater> theaters = theaterBoardService.getTheatersByLocationNo(locationNo);
+			return theaters;
+		}
+	    
 	    @GetMapping("/add")
-	    public String theaterForm() {
+	    public String theaterForm(Model model) {
+	    	List<BoardLocation> locations = theaterBoardService.getLocations();
+	    	model.addAttribute("locations", locations);
 	        return "/view/board/party/form";
 	    }
 	
--- src/main/resources/static/js/board/partyForm.js
+++ src/main/resources/static/js/board/partyForm.js
@@ -1,6 +1,4 @@
 $(function() {
-	
-
       // 오늘 날짜를 구합니다.
       let today = new Date();
 
@@ -31,7 +29,7 @@ $(function() {
 		let month = ("0" + (item.Date.getMonth() + 1)).slice(-2);
 		let day = ("0" + item.Date.getDate()).slice(-2);
 		
-		let formattedDate = year + "/" + month + "/" + day ;
+		let formattedDate = year + "-" + month + "-" + day ;
         
         $(date).attr("th:value", formattedDate)
       });
@@ -46,9 +44,81 @@ $(function() {
 		let today = new Date();
 		
 		selectedDate = $(this).attr("th:value");
-
-		alert(selectedDate)
-	})
+		theaterNo =  document.querySelector("select[name=theaterNo]");
+		movieNo =  document.querySelector("select[name=movieNo]");
+		    // AJAX 요청을 보내서 선택한 날짜에 해당하는 데이터를 가져옴
+/*
+		    $.ajax({
+	        url: '/board/party/getSchedule',
+	        method: 'GET',
+	        data: {
+	            date: selectedDate,
+	            movieNo: movieNo,
+	            theaterNo: theaterNo
+       		 },
+	        success: function(response) {
+				
+	        },
+	        error: function(xhr, status, error) {
+	            console.error(error); // 오류 메시지 출력
+	        }
+	    });
+ */
+	});
 	
 })
 
+	
+	 // 지역 멀티셀렉트
+	 	function changeLoc() {
+		let el = document.querySelector("select[name=theaterNo]");
+		el.innerHTML = "";  // select박스의 내부 컨텐츠를 전부 지운다.
+		el.disabled =false; // select박스를 활성화상태로 바꾼다.
+		let locationNo = document.querySelector("select[name=locationNo]").value
+		let xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4 && xhr.status == 200){
+				let text = xhr.responseText;
+				let theaters = JSON.parse(text);
+				if (theaters.length === 0){
+					el.disabled = true;
+				}else {
+					let options = `<option id="theater-option" value="0" selected disabled>극장선택</option>`;
+					theaters.forEach(function(theater) {
+						options += `<option id="theater-option" value="${theater.no}" >${theater.name}</option>`;
+
+					});
+					el.innerHTML = options;
+					}
+				}
+			}
+			xhr.open("GET", "theaterByLocationNo?locationNo=" + locationNo );
+			xhr.send();
+		}
+	
+		function changeTh(){
+		let el = document.querySelector("select[name=movieNo]");
+		el.innerHTML = "";  // select박스의 내부 컨텐츠를 전부 지운다.
+		el.disabled =false; // select박스를 활성화상태로 바꾼다.
+		let theaterNo = document.querySelector("select[name=theaterNo]").value
+		let xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4 && xhr.status == 200){
+				let text = xhr.responseText;
+				let theaters = JSON.parse(text);
+				if (theaters.length === 0){
+					el.disabled = true;
+				}else {
+					let options = `<option id="theater-option" value="0" selected disabled>영화선택</option>`;
+					theaters.forEach(function(movie) {
+						options += `<option id="theater-option" value="${movie.no}" >${movie.name}</option>`;
+
+					});
+					el.innerHTML = options;
+					}
+				}
+			}
+			xhr.open("GET", "movieBytheaterNo?locationNo=" + theaterNo );
+			xhr.send();
+		}
+	
