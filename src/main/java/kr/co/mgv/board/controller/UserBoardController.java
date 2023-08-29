0bbb6a7ea74aea8410d3d67d3d99235db64f99a9
신하원 <hownu080@naver.com>
2023-08-29 09:36:50 +0900

    Feat 나의MGV에 나의 게시판 메뉴 추가

--- src/main/java/kr/co/mgv/board/controller/UserBoardController.java
+++ src/main/java/kr/co/mgv/board/controller/UserBoardController.java
@@ -1,5 +1,19 @@
 package kr.co.mgv.board.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/mypage/board")
+@RequiredArgsConstructor
 public class UserBoardController {
 
+	@GetMapping("/list")
+	public String list() {
+		return "view/user/board/list";
+	}
 }
