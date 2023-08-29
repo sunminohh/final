337e08b7deacf7b8f165d4f6cb5f1c7e9d02e99b
신하원 <hownu080@naver.com>
2023-08-29 15:30:23 +0900

    Feat 나의 게시판 작성글 목록 출력 및 페이징처리

---
+++ src/main/resources/static/js/board/myboard.js
@@ -0,0 +1,25 @@
+$(function () {
+	
+	// 탭컬러 바꾸기
+	$('li.tab-link').click(function() {
+
+	      $('li.tab-link').removeClass('current');
+	      $('button.btn').removeClass('current');
+	
+	      $(this).addClass('current');
+	      $(this).find('button.btn').addClass('current');
+    });
+
+    
+    // 검색창 (글제목/내용/제목내용)
+    
+    
+    // 카테고리별(극장/영화/스토어/파티)
+    
+    
+    // 페이지
+    
+    
+    //
+	
+})
