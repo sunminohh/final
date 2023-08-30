fe6e9dd9b5132ae1c2e883b181a838fc1526352c
신하원 <hownu080@naver.com>
2023-08-30 15:32:37 +0900

    Feat 게시판관리자 페이지 검색 기능

--- src/main/resources/static/js/board/adminBoard.js
+++ src/main/resources/static/js/board/adminBoard.js
@@ -4,3 +4,26 @@ $(function() {
 		  $("#board-modal").modal('show');
 	  })
 })
+
+	function searchBoard(){	
+		let keyword = document.querySelector("input[name=keyword]").value;
+		if(keyword.trim() === ""){
+			alert("키워드를 입력하세요")			
+			document.querySelector("input[name=page]").value = 1;
+			return
+		}
+		
+		document.querySelector("input[name=page]").value = 1;
+		
+		document.querySelector("#form-board-search").submit();
+		
+	}
+	
+	function changeBoards(){
+		const boards = document.querySelector("select[name=boards]").value;
+		document.querySelector("input[name=boards]").value = boards;
+		document.querySelector("input[name=page]").value = 1;
+		
+		document.querySelector("#form-board-search").submit();
+		
+	}
