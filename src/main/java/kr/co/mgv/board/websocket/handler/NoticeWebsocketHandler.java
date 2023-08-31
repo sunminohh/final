12e09c390271249310f32075f247b8f6d3ad3bb4
신하원 <hownu080@naver.com>
2023-08-31 14:35:29 +0900

    Fix NoticeWebsocketHandler오류 해결

--- src/main/java/kr/co/mgv/board/websocket/handler/NoticeWebsocketHandler.java
+++ src/main/java/kr/co/mgv/board/websocket/handler/NoticeWebsocketHandler.java
@@ -1,7 +1,35 @@
 package kr.co.mgv.board.websocket.handler;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+@Service
 public class NoticeWebsocketHandler extends TextWebSocketHandler {
 
+	private Map<String, WebSocketSession> sessions = new ConcurrentHashMap<String, WebSocketSession>();
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		// TODO Auto-generated method stub
+		super.afterConnectionEstablished(session);
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		// TODO Auto-generated method stub
+		super.afterConnectionClosed(session, status);
+	}
+	
+	@Override
+	public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+		// TODO Auto-generated method stub
+		super.handleTransportError(session, exception);
+	}
+	
 }
