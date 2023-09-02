add04e10736d5429c0513e11b32cc5ee1b35df69
hia0706 <ljjs1215@gmail.com>
2023-09-03 00:02:28 +0900

    Feat ID 찾기 UI

---
+++ src/main/java/kr/co/mgv/user/controller/UserFindController.java
@@ -0,0 +1,81 @@
+package kr.co.mgv.user.controller;
+
+import kr.co.mgv.user.form.UserFindForm;
+import kr.co.mgv.user.service.UserService;
+import kr.co.mgv.user.vo.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+@Slf4j
+public class UserFindController {
+
+    private final UserService userService;
+
+    // todo find
+    @GetMapping("/user-find")
+    public String findForm() {
+        return "view/user/find/user-find";
+    }
+
+    @PostMapping("/user-find")
+    public ResponseEntity<?> userfind(@RequestParam String name,
+                                      @RequestParam String birth,
+                                      @RequestParam String email,
+                                      HttpSession session) {
+        User user = userService.getUserByEmail(email);
+
+        if (user != null && user.getName().equals(name) && user.getBirth().toString().equals(birth)) {
+            session.setAttribute("findUser", user);
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.badRequest().body("회원정보가 없습니다.");
+        }
+    }
+
+    @GetMapping("/user-modal")
+    public String userModal(Model model, HttpSession session) {
+        User user = (User) session.getAttribute("findUser");
+
+        if (user != null) {
+            model.addAttribute("user", user);
+        }
+        return "view/user/find/user-modal";
+    }
+
+    @GetMapping("/pwd-form")
+    public String pwdfindForm() {
+
+        return "view/user/find/pwdform";
+    }
+
+    @PostMapping("/pwd-form")
+    public String pwdfind(UserFindForm form, Model model) {
+
+
+        return "redirect:/user/auth/pass-find";
+    }
+
+    @GetMapping("pass-find")
+    public String changePwdForm(UserFindForm form, Model model) {
+
+        return "view/user/find/pass-find";
+    }
+
+    @PostMapping("/pass-find")
+    public String sucPwd() {
+
+        return "redirect:/";
+    }
+}
