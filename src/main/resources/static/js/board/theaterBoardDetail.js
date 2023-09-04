30a1b464ed08dbcab402d99880e4b034efc56446
신하원 <hownu080@naver.com>
2023-09-04 14:20:57 +0900

    style 극장게시판 댓글 width 수정

--- src/main/resources/static/js/board/theaterBoardDetail.js
+++ src/main/resources/static/js/board/theaterBoardDetail.js
@@ -140,7 +140,7 @@ $(function() {
 						
                       content += `
                       <div class="great-box">
-                      	<div class="row great-comment-box pt-1" >
+                      	<div class="row great-comment-box pt-1 ms-1" >
                               <div class="p-1 col-12" id="comment-box">
                                  <div class="d-flex justify-content-between">
                                   <div class="col-sm-11">
@@ -176,7 +176,7 @@ $(function() {
                                     </div>            
                               </div>
                            </div>   
-                           <hr>
+                           <hr style="width:1080px; ">
                         <div class="re-comment-here" id="re-comment-here-${comment.no}">
                   		</div>
                               <div class="new-register-box  row mb-3 pt-2" id="reply-comment-box-${comment.no}" >
@@ -299,7 +299,7 @@ $(function() {
 						
                       content += `
                       <div class="great-box">
-                      	<div class="row great-comment-box pt-1" >
+                      	<div class="row great-comment-box pt-1 ms-1" >
                               <div class="p-1 col-12" id="comment-box">
                                  <div class="d-flex justify-content-between">
                                   <div class="col-sm-11">
@@ -335,7 +335,7 @@ $(function() {
                                     </div>            
                               </div>
                            </div>   
-                           <hr>
+                           <hr style="width:1080px; ">
                         <div class="re-comment-here" id="re-comment-here-${comment.no}">
                   		</div>
                               <div class="new-register-box  row mb-3 pt-2" id="reply-comment-box-${comment.no}" >
