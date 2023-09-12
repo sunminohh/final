8b0d5a6579c3b8943057de880706218bb90b6c6f
hia0706 <ljjs1215@gmail.com>
2023-09-12 11:43:46 +0900

    Feat 회원 정보 홈 수정

--- src/main/java/kr/co/mgv/user/controller/UserFindController.java
+++ src/main/java/kr/co/mgv/user/controller/UserFindController.java
@@ -10,10 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
@@ -25,7 +22,6 @@ public class UserFindController {
     private final EmailServiceImpl emailService;
     private final PasswordEncoder passwordEncoder;
 
-    // todo find
     @GetMapping("/user-find")
     public String findForm() {
         return "view/user/find/user-find";
